cc923fd7316310cda35b36ae4b4547423c9d6a3c
SanNarciso <cryptic.plasmid@gmail.com>
2021-03-31 18:26:53 +1000

    All together
    UI and other

---
+++ Assets/Scripts/Catalog.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Catalog : MonoBehaviour
+{
+    public void Button1Pressed()
+    {
+        SceneManager.LoadScene("_Categories");
+    }
+}
---
+++ Assets/Scripts/Scenes.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Scenes : MonoBehaviour
+{
+    public void StartButtonPressed()
+    {
+        SceneManager.LoadScene("_Camera");
+    }
+    public void CatalogPressed()
+    {
+        SceneManager.LoadScene("_Catalog");
+    }
+    public void SmetsPressed()
+    {
+        SceneManager.LoadScene("_Smets");
+    }
+    public void CategoriesPressed()
+    {
+        SceneManager.LoadScene("_Categories");
+    }
+    public void ExitPressed()
+    {
+        Application.Quit();
+    }
+    public void menuPressed()
+    {
+        SceneManager.LoadScene("menu");
+    }
+}
