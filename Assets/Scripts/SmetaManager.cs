6e96830667b4129c335f67670131b635bca06177
Ruslan Tikhonov <59114344+tikhonow@users.noreply.github.com>
2021-04-04 07:58:51 +1000

    add smeta calc

--- Assets/Scripts/SmetaManager.cs
+++ Assets/Scripts/SmetaManager.cs
@@ -2,18 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class SmetaManager : MonoBehaviour
 {
     public static Dictionary<string,int> smeta = new Dictionary<string, int>();
     public static Text name;
     public static Text count;
+    public Text r;
     // Start is called before the first frame update
     public void PrintList() {
+        SceneManager.LoadScene("_Smets");
         foreach (KeyValuePair<string, int> kvp in smeta)
         {
-            Debug.Log(kvp.Value);
-            //textBox3.Text += string.Format("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
+            r.text += string.Format("Key = {0}, Value = {1}", kvp.Key, kvp.Value) + "\n";
         }
     }
     void Start()
